449db92e01d48b262fa0c1774e93bf996e1b3dd6
Max Litruv Boonzaayer <litruv@gmail.com>
2019-04-26 01:44:01 +1000

    wss testing

---
+++ plugins/wss.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const https = require('https');
+const WebSocket = require('ws');
+
+const fs = require("fs")
+
+
+module.exports = {
+    name: "Websockets Server (8080)",
+    init: (dclient) => {
+        client = dclient
+
+
+        const server = https.createServer({
+            cert: fs.readFileSync('./cert.pem'),
+            key: fs.readFileSync('./privkey.pem')
+        });
+        const wss = new WebSocket.Server({ server });
+
+        wss.on('connection', function connection(ws) {
+            ws.on('message', function incoming(message) {
+                console.log('received: %s', message);
+                ws.send(`something ${message}`);
+            });
+
+            ws.send('something');
+        });
+
+        server.listen(8080);
+
+    }
+}
