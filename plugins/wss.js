b6439fcea9dfea433c446100ee126427498d4a80
Max Litruv Boonzaayer <litruv@gmail.com>
2019-04-27 04:59:22 +1000

    data response

--- plugins/wss.js
+++ plugins/wss.js
@@ -6,20 +6,37 @@ const { parse, stringify } = require('flatted/cjs');
 module.exports = {
     name: "Websockets Server (8080)",
     init: (dclient) => {
-
-
         const server = https.createServer({
             cert: fs.readFileSync('./cert.pem'),
             key: fs.readFileSync('./privkey.pem')
         });
         const wss = new WebSocket.Server({ server });
 
-
         wss.on('connection', function connection(ws) {
             ws.on('message', function incoming(message) {
                 console.log('received: %s', message);
-            });
+                var data = parse(message)
 
+                var response = {
+                    type: "data",
+                    data: new Array()
+                }
+
+                if (data.type == "request") {
+                    for (var i = 0; i < data.requests.length; i++) {
+                        var resp = ""
+                        if (data.requests[i] == "name")
+                            resp = dclient.user.username
+                        if (data.requests[i] == "id")
+                            resp = dclient.user.id
+                        if (data.requests[i] == "discriminator")
+                            resp = dclient.user.discriminator
+                        response.data.push({ type: data.requests[i], data: resp })
+                    }
+
+                    ws.send(response)
+                }
+            });
             ws.send('Connected');
         });
 
@@ -30,7 +47,6 @@ module.exports = {
                     client.ping()
                 }
             });
-            console.log("Broadcasting to " + wss.clients.size + " clients")
         }, 30000);
 
         server.listen(8080);
