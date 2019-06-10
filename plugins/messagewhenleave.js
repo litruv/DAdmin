6d77d209f9795c62a151a13525f5485720446ea4
litruv <litruv@gmail.com>
2019-06-11 09:39:38 +1000

    Added a standard work week status updater

--- plugins/messagewhenleave.js
+++ plugins/messagewhenleave.js
@@ -1,6 +1,18 @@
+const moment = require("moment")
+const fs = require('fs')
+
 module.exports = {
-    name: "Auto Voice Channel",
+    name: "Nine to Five",
     init: (dclient) => {
-        return
+        client = dclient
+        setInterval(on15Update, 900000)
+        function on15Update() {
+            var currentUpdate = Math.round((moment().day() * 24 * 60 + (moment().hour() * 60) + moment().minute()) / 15)
+            fs.readFile("workweek.json", "utf8", function (err, data) {
+                let workweek = JSON.parse(data)
+                client.user.setActivity(workweek.workweek[currentUpdate], { type: 'PLAYING' })
+            })
+        }
+        on15Update()
     }
 }
