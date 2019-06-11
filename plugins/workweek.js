9f457e350709c4210f9250edc543af87e9b4e0bd
litruv <litruv@gmail.com>
2019-06-11 10:16:07 +1000

    Updated modules + afk timer

--- plugins/workweek.js
+++ plugins/workweek.js
@@ -6,7 +6,10 @@ module.exports = {
     init: (dclient) => {
         client = dclient
         moment().utcOffset(0)
+        client.user.setStatus('idle')
         setInterval(on15Update, 900000)
+
+
         function on15Update() {
             var currentUpdate = Math.round((moment().day() * 24 * 60 + (moment().hour() * 60) + moment().minute()) / 15)
             fs.readFile("workweek.json", "utf8", function (err, data) {
@@ -15,6 +18,13 @@ module.exports = {
             })
         }
         on15Update()
+
+        client.on("message", (msg) => {
+            if (msg.author.id != client.user.id) return
+            setTimeout(() => { client.user.setStatus('online') }, 100)
+
+            setTimeout(() => { client.user.setStatus('idle') }, 60000 * 5)
+        })
 
 
     }
