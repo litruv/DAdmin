706865b51dc8af0e4e7f8035e8a1148eaf5e7ab2
litruv <litruv@gmail.com>
2019-06-11 10:27:06 +1000

    playing playing games

--- plugins/workweek.js
+++ plugins/workweek.js
@@ -9,7 +9,6 @@ module.exports = {
         client.user.setStatus('idle')
         setInterval(on15Update, 900000)
 
-
         function on15Update() {
             var currentUpdate = Math.round((moment().day() * 24 * 60 + (moment().hour() * 60) + moment().minute()) / 15)
             fs.readFile("workweek.json", "utf8", function (err, data) {
@@ -17,6 +16,7 @@ module.exports = {
                 client.user.setActivity(workweek.workweek[currentUpdate], { type: 'PLAYING' })
             })
         }
+
         on15Update()
 
         client.on("message", (msg) => {
@@ -25,7 +25,5 @@ module.exports = {
 
             setTimeout(() => { client.user.setStatus('idle') }, 60000 * 5)
         })
-
-
     }
 }
